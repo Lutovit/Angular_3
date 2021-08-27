d68f7b0f5dd50251e559538bbd3b50b64e29a1fe
Lutovit <pilot_mig@bk.ru>
2021-08-27 23:47:55 +0300

    1

--- Angular_3/ClientApp/src/app/app.module.ts
+++ Angular_3/ClientApp/src/app/app.module.ts
@@ -2,22 +2,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { Routes, RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list.component';
-import { ProductDetailComponent } from './product-detail.component';
-
-// определение маршрутов
-const appRoutes: Routes = [
-    { path: '', component: ProductListComponent },
-    { path: 'product/:id', component: ProductDetailComponent },
-    { path: '**', redirectTo: '/' }
-];
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
-    declarations: [AppComponent, ProductListComponent, ProductDetailComponent],
+    imports: [BrowserModule, FormsModule, HttpClientModule],
+    declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
