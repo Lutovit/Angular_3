1ed9f484e9ed148816929856ee93fe4cc8583d19
Lutovit <pilot_mig@bk.ru>
2021-08-27 23:11:58 +0300

    Добавьте файлы проекта.

---
+++ Angular_3/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Angular_3.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
